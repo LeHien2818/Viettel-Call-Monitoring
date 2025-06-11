241ba61260d0968ecf3b48506cd213189552b182
LeHien2818 <hienltv281823@gmail.com>
2025-06-11 10:41:06 +0700

    feature(kafka): setup 2 brokers cluster

--- call-monotoring-service/app/src/main/java/call/monitor/app/config/KafkaConfiguration.java
+++ call-monotoring-service/app/src/main/java/call/monitor/app/config/KafkaConfiguration.java
@@ -18,7 +18,7 @@ public class KafkaConfiguration {
     @Bean
     public ConsumerFactory<String, String> consumerFactory() {
         Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "172.17.0.1:9092");
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092,localhost:9093");
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "call-app-consumer");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, GsonDeserializer.class);
